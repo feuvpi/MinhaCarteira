e149cc4d8da39a1094cec8974ed833b4ec8eb271
feuvpi <fredvpg@gmail.com>
2022-04-14 19:49:11 -0300

    initial commit.

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+//Import ROUTES
+const operationsRoute = require("./routes/operations");
+
+//Initialize express
+const app = express();
+
+//Initializing Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Setting up Routes
+app.use("/operations", operationsRoute);
+
+//Connect to database
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("Connected to database.");
+});
+
+//Start listening to the server
+app.listen(3000);
+
+//APPLICATION ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+/*
+
+- Instalado a dependência bodyParser para ler dados em JSON passados pelo req.body
+
+*/
