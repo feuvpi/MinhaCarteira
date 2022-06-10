fbb355ecd510d6ecdd2ccda768cb4f252245d7a0
feuvpi <fredvpg@gmail.com>
2022-06-10 10:44:51 -0300

    Criado pasta config com arquivo json para armazenameto de dados jwt.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
