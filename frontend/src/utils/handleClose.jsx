036798da653331e5a53f3fbcb8d71f3ec584866b
feuvpi <fredvpg@gmail.com>
2022-08-15 00:34:29 -0300

    criada funcionalidade para fechar o modal de operacao quando clicado fora.

---
+++ frontend/src/utils/handleClose.jsx
@@ -0,0 +1,27 @@
+// -- react
+import React, { useState } from "react";
+
+ export const handleClose = (e, show) => {
+    if(e.target.id === "Modal"){
+        return true;
+    }
+    if(e.target.id !== "addOperation"){
+        if(!show){
+            console.log("aqui")
+            console.log(e.target.id)
+            return false
+        } else{
+            return null
+        }
+    }
+    if(!show){
+        console.log("aqui2")
+        return true;
+        
+    } else{
+        console.log("aqui3")
+        return false
+        console.log(show)
+        console.log(e.target)
+    }
+}
